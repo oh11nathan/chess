ee959d7b57a44209b075325714b28a1789f110e0
Nathan <oh11nathan@gmail.com>
2021-04-26 08:31:51 -0400

    King valid moves functionality.

--- lib/king.rb
+++ lib/king.rb
@@ -1,5 +1,11 @@
 
+require_relative 'general_moves'
+
 class King
+
+    include GeneralMoves
+
+    attr_reader :valid_moves
 
     def initialize(colour, position)
         @colour = colour
@@ -20,7 +26,32 @@ class King
     end
 
     def king_valid_moves(position)
+        res = Array.new
+
+        res.push(return_diagonal(position, "nw")[0])
+        res.push(return_diagonal(position, "ne")[0])
+        res.push(return_diagonal(position, "se")[0])
+        res.push(return_diagonal(position, "sw")[0])
+
+        res.push(return_cross(position, "n")[0])
+        res.push(return_cross(position, "e")[0])
+        res.push(return_cross(position, "s")[0])
+        res.push(return_cross(position, "w")[0])
+
+        return res
+    end
+
+    def adjacent_moves(arr, position)
+        # get node from board
+        # create function to make all the 
 
     end
 
 end
+
+# some smoke test
+
+king = King.new("black", "B2")
+
+print king.valid_moves
+print "\n"
