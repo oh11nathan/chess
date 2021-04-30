bc22ccf8cb0fa3c05291353c38c2ce0deceb6e57
Nathan <oh11nathan@gmail.com>
2021-04-30 15:53:38 -0400

    Comment removal from spec files.

--- spec/02_bishop_spec.rb
+++ spec/02_bishop_spec.rb
@@ -1,10 +1,6 @@
 
 require_relative '../lib/bishop'
 
-# describe -> Name of the method that is being tested.
-# context ->  Explains the conditions of the test.
-# it ->       Explains the results of the test.
-
 describe Bishop do
 
 end
